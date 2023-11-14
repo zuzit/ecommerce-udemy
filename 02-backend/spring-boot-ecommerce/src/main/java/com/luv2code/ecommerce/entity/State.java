76533d03cb117e60f37be7639cad4859d79d4900
Zuzana Tuskova <tuskovazuzan@gmail.com>
2023-11-14 17:03:51 +0100

    backend - entities added

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/State.java
@@ -0,0 +1,22 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name ="state")
+@Data
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+}
